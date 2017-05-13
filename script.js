a28d7a33e0137ad260cc602ecf515892bf2cfae5
Michael Pimentel <mtpjr88@gmail.com>
2017-05-13 15:00:45 -0700

    added click functions

--- script.js
+++ script.js
@@ -5,8 +5,20 @@ function playSound(e) {
   audio.currentTime = 0; // allows the sound to start again or repeat when pressed
   audio.play();
   key.classList.add('playing'); // adds animation from  my css
-
+  console.log(audio);
 };
+
+function clickSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+   
+};
+
+	function clap(){
+		const clap = document.querySelector("audio[data-key='65']");
+		clap.currentTime = 0;
+		clap.play();
+	}
 
 function removeTransition(e) {
   if(e.propertyName !== 'transform') return; // skip it if it's not a transform
@@ -17,3 +29,5 @@ const keys = document.querySelectorAll('.key');
 keys.forEach(key => key.addEventListener('transitionend',removeTransition));
 
 window.addEventListener('keydown', playSound);
+
+window.addEventListener('click', clickSound);
