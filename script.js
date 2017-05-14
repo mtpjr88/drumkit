c5cd76eecf883a1916ca7e5c65432b0fcca58787
Michael Pimentel <mtpjr88@gmail.com>
2017-05-13 21:55:39 -0700

    added animation while it is clicked

--- script.js
+++ script.js
@@ -13,46 +13,65 @@ function playSound(e) {
 		const clap = document.querySelector("audio[data-key='65']");
 		clap.currentTime = 0;
 		clap.play();
+		const key = document.querySelector(".key[data-key='65']");
+		key.classList.add('playing'); // adds the css animation while its playing
 	}
 	function hihat() {
 		const hihat = document.querySelector("audio[data-key='83']");
 		hihat.currentTime = 0;
 		hihat.play();
+		const key = document.querySelector(".key[data-key='83']");
+		key.classList.add('playing');
 	}
 	function kick() {
 		const kick = document.querySelector("audio[data-key='68']");
 		kick.currentTime = 0;
 		kick.play();
+		const key = document.querySelector(".key[data-key='68']");
+		key.classList.add('playing');
 	}
 	function openhat() {
 		const openhat = document.querySelector("audio[data-key='70']");
 		openhat.currentTime = 0;
 		openhat.play();
+		const key = document.querySelector(".key[data-key='70']");
+		key.classList.add('playing');
 	}
 	function boom() {
 		const boom = document.querySelector("audio[data-key='71']");
 		boom.currentTime = 0;
 		boom.play();
+		const key = document.querySelector(".key[data-key='71']");
+		key.classList.add('playing');
 	}
 	function ride() {
 		const ride = document.querySelector("audio[data-key='72']");
 		ride.currentTime = 0;
 		ride.play();
+		const key = document.querySelector(".key[data-key='72']");
+		key.classList.add('playing');
 	}
 	function snare() {
 		const snare = document.querySelector("audio[data-key='74']");
 		snare.currentTime = 0;
 		snare.play();
+		const key = document.querySelector(".key[data-key='74']");
+		key.classList.add('playing');
 	}
 	function tom() {
 		const tom = document.querySelector("audio[data-key='75']");
 		tom.currentTime = 0;
 		tom.play();
+		const key = document.querySelector(".key[data-key='75']");
+		key.classList.add('playing');
 	}
 	function tink() {
 		const tink = document.querySelector("audio[data-key='76']");
 		tink.currentTime = 0;
 		tink.play();
+		const key = document.querySelector(".key[data-key='76']");
+		key.classList.add('playing');
+
 	}
 
 function removeTransition(e) {
