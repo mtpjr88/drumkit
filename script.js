b39d2a918e71427898441b13a68167344e002e43
Michael Pimentel <mtpjr88@gmail.com>
2017-05-13 21:43:26 -0700

    added click functions

--- script.js
+++ script.js
@@ -8,16 +8,51 @@ function playSound(e) {
   console.log(audio);
 };
 
-function clickSound(e) {
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-   
-};
-
-	function clap(){
+	// Onclick functions
+	function clap() {
 		const clap = document.querySelector("audio[data-key='65']");
 		clap.currentTime = 0;
 		clap.play();
+	}
+	function hihat() {
+		const hihat = document.querySelector("audio[data-key='83']");
+		hihat.currentTime = 0;
+		hihat.play();
+	}
+	function kick() {
+		const kick = document.querySelector("audio[data-key='68']");
+		kick.currentTime = 0;
+		kick.play();
+	}
+	function openhat() {
+		const openhat = document.querySelector("audio[data-key='70']");
+		openhat.currentTime = 0;
+		openhat.play();
+	}
+	function boom() {
+		const boom = document.querySelector("audio[data-key='71']");
+		boom.currentTime = 0;
+		boom.play();
+	}
+	function ride() {
+		const ride = document.querySelector("audio[data-key='72']");
+		ride.currentTime = 0;
+		ride.play();
+	}
+	function snare() {
+		const snare = document.querySelector("audio[data-key='74']");
+		snare.currentTime = 0;
+		snare.play();
+	}
+	function tom() {
+		const tom = document.querySelector("audio[data-key='75']");
+		tom.currentTime = 0;
+		tom.play();
+	}
+	function tink() {
+		const tink = document.querySelector("audio[data-key='76']");
+		tink.currentTime = 0;
+		tink.play();
 	}
 
 function removeTransition(e) {
@@ -30,4 +65,4 @@ keys.forEach(key => key.addEventListener('transitionend',removeTransition));
 
 window.addEventListener('keydown', playSound);
 
-window.addEventListener('click', clickSound);
+
